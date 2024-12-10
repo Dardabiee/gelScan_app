2c1e0f9b8b81136c59db43b5b9d41ca95d709739
Muhammad Darda Fathammubienaa <muhammaddarda733@gmail.com>
2024-12-10 10:37:18 +0700

    update ui

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -3,77 +3,189 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // backgroundColor: Colors.transparent,
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: (){},
-          )
-        ],
-      ),
-      body: Column(
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      elevation: 0,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.settings),
+          onPressed: () {},
+        ),
+      ],
+    ),
+    body: SingleChildScrollView(  // Membungkus hanya bagian kolom yang ingin di-scroll
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(padding: EdgeInsets.symmetric(horizontal: 40),
-          child: Row(
-            children: [
-              CircleAvatar(
-                radius: 30,
-                backgroundImage: AssetImage('assets/images/gojo.jpeg'),
-              ),
-              SizedBox(width: 10,),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Halo, Gojo sitorus ', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                  Text('Frontend Developer', style: TextStyle(fontSize: 16, color: Colors.grey, fontWeight: FontWeight.w600)),
-                ],
-              )
-            ],
-           ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/images/gojo.jpeg'),
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Halo, Gojo Sitorus ',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Frontend Developer',
+                      style: TextStyle(fontSize: 16, color: Colors.grey, fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-          SizedBox(height: 20,),
-          Padding(padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Welcome to",style: TextStyle(fontSize: 20),),
-              Text("gelScan",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            ],
+          const SizedBox(height: 40),
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/scan');
+            },
+            child: Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text.rich(
+                              TextSpan(
+                                text: "Scan smarter, anywhere\n",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22,
+                                  color: Colors.white,
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: "Enjoy before it runs out!",
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 100, height: 100),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 10,
+                  right: 40,
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/scan');
+                    },
+                    icon: Icon(
+                      Icons.qr_code_scanner,
+                      color: Colors.white,
+                      size: 100,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                BuildButton(
+                    icon: Icons.send,
+                    label: 'Send',
+                    iconColor: Colors.white,
+                    backgroundColor: Colors.lightBlue,
+                    onTap: () {}),
+                BuildButton(
+                    icon: Icons.print,
+                    label: 'Print',
+                    iconColor: Colors.white,
+                    backgroundColor: Colors.lightBlue,
+                    onTap: () {}),
+              ],
+            ),
           ),
-          SizedBox(height: 50,),
-          Expanded(
-            child: GridView.count(
-            crossAxisCount: 2,
-            childAspectRatio: 1,
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 5,
-            children: [
-              BuildButton(icon: Icons.qr_code, label:'Create', iconColor: Colors.white, backgroundColor: Colors.lightBlue, onTap: (){
-                Navigator.pushNamed(context, '/create');
-              }),
-              BuildButton(icon: Icons.qr_code_scanner, label:'Scan', iconColor: Colors.white, backgroundColor: Colors.lightBlue, onTap: (){
-                Navigator.pushNamed(context, '/scan');
-              }),
-              BuildButton(icon: Icons.send, label:'Send', iconColor: Colors.white, backgroundColor: Colors.lightBlue, onTap: (){}),
-              BuildButton(icon: Icons.print, label:'Print', iconColor: Colors.white, backgroundColor: Colors.lightBlue, onTap: (){}),
-
-            ],
-           )
-          )
+          SizedBox(height: 100,),
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/create');
+            },
+            child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Row(
+                      children: [
+                        Text('create QR code ',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.w400)),
+                        Icon(Icons.qr_code, color: Colors.white,)
+                      ],
+                    ),
+                  ),
+          ),
         ],
       ),
+    ),
+    bottomNavigationBar: NavigationBar(),
+  );
+}
+
+}
+
+class NavigationBar extends StatefulWidget {
+  const NavigationBar({
+    super.key,
+  });
+
+  @override
+  State<NavigationBar> createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  int _currentIndex = 0;
+  List<Widget> body = const[
+    Icon(Icons.home),
+    Icon(Icons.menu),
+    Icon(Icons.person)
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      items: const[
+        BottomNavigationBarItem(label: 'Home',icon: Icon(Icons.home,color: Colors.lightBlue,),),
+        BottomNavigationBarItem(label: 'Menu',icon: Icon(Icons.menu),),
+        BottomNavigationBarItem(label: 'Person',icon: Icon(Icons.person),),
+      ],
     );
   }
 }
-
 class BuildButton extends StatelessWidget {
   const BuildButton({
     super.key,
@@ -95,8 +207,8 @@ class BuildButton extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width: 110, // Lebar tetap untuk setiap tombol
-        height: 110, // Tinggi tetap untuk setiap tombol
+        width: 170, // Lebar tetap untuk setiap tombol
+        height: 170, // Tinggi tetap untuk setiap tombol
         decoration: BoxDecoration(
           color: Colors.white, // Latar tombol abu-abu terang
           borderRadius: BorderRadius.circular(20), // Sudut membulat
